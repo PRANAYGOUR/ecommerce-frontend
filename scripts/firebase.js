a4d2615fa4470bd5d436035647029f535190541f
PRANAYGOUR <pranaygour3042007@gmail.com>
2025-12-25 01:17:48 +0530

    completed

---
+++ scripts/firebase.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB19bXCvRhROEGcjLXSSQSK9mo-TTUabvs",
+    authDomain: "ecommerce-frontend-7d9cc.firebaseapp.com",
+    projectId: "ecommerce-frontend-7d9cc",
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
